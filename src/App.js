66d21e8604628205d704ebe21fa627d27dbba8e0
MaxKayan <maxim.kayander1@gmail.com>
2020-12-14 16:47:27 +0300

    Lesson 3.1

--- src/App.js
+++ src/App.js
@@ -1,37 +1,50 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 0;
+    background-color: #f0f0f0;
+    font-family: Roboto, sans-serif;
+    font-size: 20px;
+    color: black;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  h1, h2, h3 {
+    font-family: Pacifico;
+    padding: 0;
+    margin: 0;
+  }
+`;
 
 function App() {
-  const sleep = (ms) =>
-    new Promise((resolve) => {
-      setTimeout(resolve, ms);
-    });
-
-  sleep(3000)
-    .then((value) => {
-      console.log("Done", value);
-    })
-    .catch((reason) => {
-      console.log(reason);
-    })
-    .finally();
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Nice!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GlobalStyle />
+      <div className="App">
+        <h1>Hello React</h1>
+      </div>
     </div>
   );
 }
